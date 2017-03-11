36df7a73d33a852201ccf968869e40c08c3f0092
Florian Vick <florian.vick@comparat.de>
2017-03-11 11:15:19 +0100

    Editieren von Medien implementiert

--- handlers/books.go
+++ handlers/books.go
@@ -62,112 +62,9 @@ func HandleBooksAddPOST (w http.ResponseWriter, r *http.Request) {
 	db := context.Get(r, "db").(*mgo.Database)
 	// Now lets perform a count query using mgo db object.
 
-	fPublishedDate, errFP := time.Parse("yyyy-MM-dd", r.PostFormValue("booksFirstPublished"))
-
-	if errFP != nil {
-		fmt.Println("[AddBook] Published Date failed to convert -> " + errFP.Error())
-		fPublishedDate = time.Date(1970, 1,1, 0,0,0,0, time.Local)
-	}
-
-	lCheckedDate, errLC := time.Parse("yyyy-MM-dd", r.PostFormValue("bookLastChecked"))
-
-	if errLC != nil {
-		fmt.Println("[AddBook] Last Checked date failed to convert -> " + errLC.Error())
-		lCheckedDate = time.Now()
-	}
-
-	edition, errEdition := strconv.Atoi(r.PostFormValue("booksEdition"))
-
-	if errEdition != nil {
-		fmt.Println("[AddBook] Edition failed to convert -> " + errEdition.Error())
-		edition = 1
-	}
-
-	isbn, errISBN := strconv.Atoi(r.PostFormValue("bookISBN"))
-
-	if errISBN != nil {
-		fmt.Println("[AddBook] ISBN failed to convert -> " + errISBN.Error())
-		isbn = 0
-	}
-
-	lineBookNr, errLBN := strconv.Atoi(r.PostFormValue("bookLineNumber"))
-
-	if errLBN != nil {
-		fmt.Println("[AddBook] LineBookNr failed to convert -> " + errLBN.Error())
-		lineBookNr = 0
-	}
-
-	numOfB, errNOB := strconv.Atoi(r.PostFormValue("bookNumberOfBooks"))
-
-	if errNOB != nil {
-		fmt.Println("[AddBook] numOfB failed to convert -> " + errNOB.Error())
-		numOfB = 1
-	}
-
-	price, errPrice := strconv.ParseFloat(r.PostFormValue("bookPrice"), 64)
-
-	if errPrice != nil {
-		fmt.Println("[AddBook] Price failed to convert -> " + errPrice.Error())
-		price = 0.0
-	}
-
-	rating, errRating := strconv.Atoi(r.PostFormValue("booksRating"))
-
-	if errRating != nil {
-		fmt.Println("[AddBook] Rating failed to convert -> " + errRating.Error())
-		rating = 0
-	}
-
-	t, errT := strconv.Atoi(r.PostFormValue("bookType"))
-
-	if errT != nil {
-		fmt.Println("[AddBook] Book Type failed to convert -> " + errT.Error())
-		t = 0
-	}
-
-	year, errYear := strconv.Atoi(r.PostFormValue("bookYear"))
-
-	if errYear != nil {
-		fmt.Println("[AddBook] Year failed to convert -> " + errYear.Error())
-		year = 1970
-	}
-
-	b := models.Book{
-		Title: 			r.PostFormValue("bookTitle"),
-		Abstract: 		r.PostFormValue("bookAbstract"),
-		AdditionalUrl: 		r.PostFormValue("bookAddUrl"),
-		AddTitle: 		r.PostFormValue("bookAddTitle"),
-		AuthoringAssistants: 	r.PostFormValue("bookAuthoringAssistants"),
-		Authors: 		r.PostFormValue("bookAuthor"),
-		Categories:		strings.Split(r.PostFormValue("bookCategories"), ","),
-		DateAdded:		time.Now(),
-		DateModified:		time.Now(),
-		Edition:		edition,
-		FirstPublished:		fPublishedDate,
-		Institution:		r.PostFormValue("bookInstitution"),
-		ISBN:			isbn,
-		Language:		r.PostFormValue("bookLanguage"),
-		LastChecked:		lCheckedDate,
-		LineBookNo:		lineBookNr,
-		LineTitle:		r.PostFormValue("bookLineTitle"),
-		Notes:			r.PostFormValue("bookNotes"),
-		NumberOfBooks:		numOfB,
-		Price:			price,
-		Publisher:		r.PostFormValue("bookPublisher"),
-		PublishingLocation:	r.PostFormValue("bookPublishingLocation"),
-		Locations:		make([]models.Location, 0),
-		Quotations:		make([]models.Quotation, 0),
-		Rating:			rating,
-		References:		strings.Split(r.PostFormValue("bookReferences"), ","),
-		SubTitle:		r.PostFormValue("bookSubtitle"),
-		Tags:			strings.Split(r.PostFormValue("bookTags"), ","),
-		Tasks:			make([]models.Task, 0),
-		ToC:			r.PostFormValue("bookToc"),
-		Type:			t,
-		Year:			year,
-	}
-	b.Id = bson.NewObjectId().Hex()
-	err := db.C("books").Insert(&b)
+	book := BookObjFromPost(r)
+	book.Id = bson.NewObjectId().Hex()
+	err := db.C("books").Insert(&book)
 
 	if err != nil {
 		panic(err)
@@ -277,6 +174,34 @@ func HandleBooksEditGET (w http.ResponseWriter, r *http.Request) {
 
 func HandleBooksEditPOST (w http.ResponseWriter, r *http.Request) {
 
+	pData := models.PageBookEdit{}
+	if len(r.URL.Path) < len("/books/edit/a") {
+		pData.Error = "Kein Werk ausgewählt!"
+		pData.Title = "Fehler"
+	} else {
+		pData.Title = "Werk editieren"
+
+		bookId := r.URL.Path[len("/books/edit/"):]
+
+		db := context.Get(r, "db").(*mgo.Database)
+
+		book := BookObjFromPost(r)
+		book.Id = bookId
+
+		err := db.C("books").Update(bson.M{ "_id": bookId }, &book)
+
+		if err != nil {
+			pData.Error = "Datensatz konnte nicht gespeichert werden: " + err.Error()
+		} else {
+			pData.Error = "Datensatz gespeichert."
+		}
+
+		pData.Book = book
+		pData.GenCheckStr()
+
+	}
+	tpl, _ := template.ParseGlob("html/*.html")
+	tpl.ExecuteTemplate(w, "books-edit.html", pData)
 }
 
 func HandleBooksIndex (w http.ResponseWriter, r *http.Request) {
@@ -288,4 +213,112 @@ func HandleBooksIndex (w http.ResponseWriter, r *http.Request) {
 	}
 
 	http.Redirect(w, r, "/books/list", 302)
+}
+
+func BookObjFromPost(r *http.Request) models.Book {
+	fPublishedDate, errFP := time.Parse("yyyy-MM-dd", r.PostFormValue("booksFirstPublished"))
+
+	if errFP != nil {
+		fmt.Println("[AddBook] Published Date failed to convert -> " + errFP.Error())
+		fPublishedDate = time.Date(1970, 1,1, 0,0,0,0, time.Local)
+	}
+
+	lCheckedDate, errLC := time.Parse("yyyy-MM-dd", r.PostFormValue("bookLastChecked"))
+
+	if errLC != nil {
+		fmt.Println("[AddBook] Last Checked date failed to convert -> " + errLC.Error())
+		lCheckedDate = time.Now()
+	}
+
+	edition, errEdition := strconv.Atoi(r.PostFormValue("booksEdition"))
+
+	if errEdition != nil {
+		fmt.Println("[AddBook] Edition failed to convert -> " + errEdition.Error())
+		edition = 1
+	}
+
+	isbn, errISBN := strconv.Atoi(r.PostFormValue("bookISBN"))
+
+	if errISBN != nil {
+		fmt.Println("[AddBook] ISBN failed to convert -> " + errISBN.Error())
+		isbn = 0
+	}
+
+	lineBookNr, errLBN := strconv.Atoi(r.PostFormValue("bookLineNumber"))
+
+	if errLBN != nil {
+		fmt.Println("[AddBook] LineBookNr failed to convert -> " + errLBN.Error())
+		lineBookNr = 0
+	}
+
+	numOfB, errNOB := strconv.Atoi(r.PostFormValue("bookNumberOfBooks"))
+
+	if errNOB != nil {
+		fmt.Println("[AddBook] numOfB failed to convert -> " + errNOB.Error())
+		numOfB = 1
+	}
+
+	price, errPrice := strconv.ParseFloat(r.PostFormValue("bookPrice"), 64)
+
+	if errPrice != nil {
+		fmt.Println("[AddBook] Price failed to convert -> " + errPrice.Error())
+		price = 0.0
+	}
+
+	rating, errRating := strconv.Atoi(r.PostFormValue("booksRating"))
+
+	if errRating != nil {
+		fmt.Println("[AddBook] Rating failed to convert -> " + errRating.Error())
+		rating = 0
+	}
+
+	t, errT := strconv.Atoi(r.PostFormValue("bookType"))
+
+	if errT != nil {
+		fmt.Println("[AddBook] Book Type failed to convert -> " + errT.Error())
+		t = 0
+	}
+
+	year, errYear := strconv.Atoi(r.PostFormValue("bookYear"))
+
+	if errYear != nil {
+		fmt.Println("[AddBook] Year failed to convert -> " + errYear.Error())
+		year = 1970
+	}
+
+	b := models.Book{
+		Title: 			r.PostFormValue("bookTitle"),
+		Abstract: 		r.PostFormValue("bookAbstract"),
+		AdditionalUrl: 		r.PostFormValue("bookAddUrl"),
+		AddTitle: 		r.PostFormValue("bookAddTitle"),
+		AuthoringAssistants: 	r.PostFormValue("bookAuthoringAssistants"),
+		Authors: 		r.PostFormValue("bookAuthor"),
+		Categories:		strings.Split(r.PostFormValue("bookCategories"), ","),
+		DateAdded:		time.Now(),
+		DateModified:		time.Now(),
+		Edition:		edition,
+		FirstPublished:		fPublishedDate,
+		Institution:		r.PostFormValue("bookInstitution"),
+		ISBN:			isbn,
+		Language:		r.PostFormValue("bookLanguage"),
+		LastChecked:		lCheckedDate,
+		LineBookNo:		lineBookNr,
+		LineTitle:		r.PostFormValue("bookLineTitle"),
+		Notes:			r.PostFormValue("bookNotes"),
+		NumberOfBooks:		numOfB,
+		Price:			price,
+		Publisher:		r.PostFormValue("bookPublisher"),
+		PublishingLocation:	r.PostFormValue("bookPublishingLocation"),
+		Locations:		make([]models.Location, 0),
+		Quotations:		make([]models.Quotation, 0),
+		Rating:			rating,
+		References:		strings.Split(r.PostFormValue("bookReferences"), ","),
+		SubTitle:		r.PostFormValue("bookSubtitle"),
+		Tags:			strings.Split(r.PostFormValue("bookTags"), ","),
+		Tasks:			make([]models.Task, 0),
+		ToC:			r.PostFormValue("bookToc"),
+		Type:			t,
+		Year:			year,
+	}
+	return b
 }
